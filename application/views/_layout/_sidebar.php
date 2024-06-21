4923e561da9fb5e26c70d464928f743983807c1d
aurelioo29 <aureliolucio5@gmail.com>
2024-06-21 09:47:46 +0700

    add class into Selection navbar Transaksi

--- application/views/_layout/_sidebar.php
+++ application/views/_layout/_sidebar.php
@@ -20,12 +20,8 @@
 
       <!-- Data Transaksi -->
       <li class="treeview">
-        <a href="#">
-          <i class="glyphicon glyphicon-th-list"></i>
-          <span>Pembelian Barang</span>
-          <i class="fa fa-angle-left pull-right"></i>
-        </a>
-        <ul>
+        <a href="#"><i class="glyphicon glyphicon-th-list"></i> <span>Pembelian Barang</span><i class="fa fa-angle-left pull-right"></i></a>
+        <ul class="treeview-menu">
           <li><a href="<?php echo base_url() ?>supplier"><i class="fa fa-circle-o"></i> Data Supplier</a></li>
           <li><a href="<?php echo base_url() ?>pembelian/tambah"><i class="fa fa-circle-o"></i> Pembelian Baru</a></li>
           <li><a href="<?php echo base_url() ?>pembelian"><i class="fa fa-circle-o"></i> Data Pembelian</a></li>
